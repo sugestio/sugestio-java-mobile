3ed818884417a7fcfa2bad05a43395a273bec573
Kris <github@sugestio.com>
2010-12-15 22:36:59 +0100

    get recommendations

--- src/com/sugestio/client/SugestioClient.java
+++ src/com/sugestio/client/SugestioClient.java
@@ -2,13 +2,14 @@ package com.sugestio.client;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
@@ -17,6 +18,7 @@ import org.apache.http.util.EntityUtils;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 
 public class SugestioClient {
@@ -24,8 +26,7 @@ public class SugestioClient {
 	private String baseUri = "http://api.sugestio.com";
 	private HttpClient httpClient;
 	private String account;
-	private ResponseHandler<byte[]> handler;
-	
+		
 	
 	/**
 	 * Creates a new instance of the SugestioClient with the given access credentials.
@@ -35,6 +36,41 @@ public class SugestioClient {
 		this.account = account;
 		this.httpClient = new DefaultHttpClient();			
 	}
+	
+	/**
+	 * Get personal recommendations for the given user.
+	 * @param userid the user
+	 * @param parameters query parameters
+	 * @return recommendations
+	 * @throws Exception
+	 */
+	public JsonArray getRecommendations(String userid, Map<String, String> parameters) throws Exception {
+		
+		JsonElement response = doGet("/users/" + userid + "/recommendations.json", parameters, false); 
+		
+		if (response != null)
+			return response.getAsJsonArray();
+		else
+			return new JsonArray();
+		
+	}
+	
+	/**
+	 * Get similar item recommendations for the given item.
+	 * @param itemid the item
+	 * @param parameters query parameters
+	 * @return similar item recommendations
+	 * @throws Exception
+	 */
+	public JsonArray getSimilarItems(String itemid, Map<String, String> parameters) throws Exception {
+		
+		JsonElement response = doGet("/users/" + itemid + "/similar.json", parameters, false); 
+		
+		if (response != null)
+			return response.getAsJsonArray();
+		else
+			return new JsonArray();
+	} 
 	
 	/**
 	 * Submit a consumption.
@@ -63,9 +99,34 @@ public class SugestioClient {
 		return this.doPost("/users", user);
 	}
 
-	private JsonObject doGet(String resource, List<NameValuePair> parameters) {
+	private JsonElement doGet(String resource, Map<String, String> parameters, boolean raise404) throws Exception {
+		
+		List<NameValuePair> queryParams = new ArrayList<NameValuePair>(); // todo
 		//URLEncodedUtils.format(null, "UTF-8");
-		return null;
+		
+		try {
+			
+			HttpGet httpGet = new HttpGet(getUri(resource));
+			HttpResponse httpResponse = httpClient.execute(httpGet);
+			String body = EntityUtils.toString(httpResponse.getEntity());
+			int code = httpResponse.getStatusLine().getStatusCode();
+			
+			if (code == 200) {			
+				JsonParser parser = new JsonParser();				
+				return parser.parse(body);
+			} else if (code == 404 && !raise404) {
+				return null;
+			} else {
+				String message = "Response code " + httpResponse.getStatusLine().getStatusCode() + ". ";
+				message += body; 
+				throw new Exception(message);
+			}
+			
+		} catch (Exception e) {
+			httpClient.getConnectionManager().shutdown();
+			throw e;
+		}		
+		
 	}
 	
 	/**
